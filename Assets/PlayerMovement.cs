efc46655b2be7a1d4537313b848634fe2c53ffa6
Francisco Castro <francisco.castro@sooper.com.br>
2022-04-03 19:43:17 -0300

    Refactor to implement ControllerKey class

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,11 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     [Header("Controller Keys")]
-    public KeyCode upKey = KeyCode.W;
-    public KeyCode leftKey = KeyCode.A;
-    public KeyCode downKey = KeyCode.S;
-    public KeyCode rightKey = KeyCode.D;
-    public KeyCode sprintKey = KeyCode.LeftShift;
+    public ControllerKey[] controllerKeys;
 
     [Header("Movement")]
     public float speed = 5f;
@@ -24,53 +21,38 @@ public class PlayerMovement : MonoBehaviour
     public float delay = 1f;
     public float chargeSpeed = 1f;
 
-    [HideInInspector]    
-    public bool delayUp = true;
-    [HideInInspector]    
-    public bool delayLeft = true;
-    [HideInInspector]    
-    public bool delayDown = true;
-    [HideInInspector]
-    public bool delayRight = true;
-
     private Dictionary<string, float> delayLevel = new Dictionary<string, float>();
-    private List<KeyCode> directionKeyCodes;
-    private Dictionary<string, KeyCode> directionKeyCodesMap = new Dictionary<string, KeyCode>();
     private Dictionary<string, Vector3> keyCode2VectorMap = new Dictionary<string, Vector3>();
-    private Dictionary<string, bool> keyCode2DelayStatus = new Dictionary<string, bool>();
+    private Dictionary<string, ControllerKey> controllerKeyMap = new Dictionary<string, ControllerKey>();
 
     private void Start()
     {
-        directionKeyCodes = new List<KeyCode>
-        {
-            upKey,
-            leftKey,
-            downKey,
-            rightKey
-        };
+        keyCode2VectorMap.Add(KeyLabel.up.ToString(), Vector3.forward);
+        keyCode2VectorMap.Add(KeyLabel.left.ToString(), Vector3.left);
+        keyCode2VectorMap.Add(KeyLabel.down.ToString(), Vector3.back);
+        keyCode2VectorMap.Add(KeyLabel.right.ToString(), Vector3.right);
 
-        directionKeyCodes.ForEach(keyCode => {
-            string directionLabel = keyCode.ToString();
-            directionKeyCodesMap.Add(directionLabel, keyCode);
-            delayLevel.Add(directionLabel, 0f);
+        controllerKeys.ToList().ForEach(controllerKey => {
+            string keyLabel = controllerKey.keyLabel.ToString();
+            controllerKeyMap.Add(keyLabel, controllerKey);
+
+            if (keyCode2VectorMap.ContainsKey(keyLabel))
+            {
+                controllerKey.direction = keyCode2VectorMap[keyLabel];
+            }
+
+            delayLevel.Add(keyLabel, 0f);
         });
-
-        keyCode2VectorMap.Add(upKey.ToString(), Vector3.forward);
-        keyCode2VectorMap.Add(leftKey.ToString(), Vector3.left);
-        keyCode2VectorMap.Add(downKey.ToString(), Vector3.back);
-        keyCode2VectorMap.Add(rightKey.ToString(), Vector3.right);
     }
 
     void FixedUpdate()
     {
         Vector3 velocityDir = Vector3.zero;
         float velocityMag = speed;
-        for (int i = 0; i < directionKeyCodes.Count; i++)
-        {
-            KeyCode directionKeyCode = directionKeyCodes[i];
-            string directionLabel = directionKeyCode.ToString();
-            velocityDir = HandleDirection(velocityDir, directionKeyCode, keyCode2VectorMap[directionLabel]);
-        }
+        controllerKeys.ToList().ForEach(controllerKey => {
+            string keyLabel = controllerKey.keyLabel.ToString();
+            velocityDir = HandleDirection(velocityDir, controllerKey.keyCode, controllerKey.direction);
+        });
         velocityMag = HandleSprint(velocityMag);
         rb.velocity = new Vector3(velocityDir.x, 0, velocityDir.z).normalized * velocityMag + Vector3.up * rb.velocity.y;
     }
@@ -88,7 +70,8 @@ public class PlayerMovement : MonoBehaviour
 
     private float HandleSprint(float velocityMag)
     {
-        if (Input.GetKey(sprintKey) && enableSprint)
+        ControllerKey sprintControllerKey = controllerKeyMap[KeyLabel.sprint.ToString()];
+        if (Input.GetKey(sprintControllerKey.keyCode) && enableSprint)
         {
             velocityMag = sprint;
         }
@@ -106,42 +89,36 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        directionKeyCodes.ForEach(keyCode => ChargeDirection(keyCode));
+        controllerKeys.ToList().ForEach(ChargeKey);
     }
 
-    private void ChargeDirection(KeyCode keyCode)
+    private void ChargeKey(ControllerKey controllerKey)
     {
-        string directionLabel = keyCode.ToString();
-
-        KeyCode directionKeyCode = directionKeyCodesMap[directionLabel];
-        if (!Input.GetKey(directionKeyCode))
+        string keyLabel = controllerKey.keyLabel.ToString();
+        if (!Input.GetKey(controllerKey.keyCode))
         {
-            delayLevel[directionLabel] = 0f;
+            delayLevel[keyLabel] = 0f;
             return;
         }
 
-        if (delayLevel[directionLabel] >= delay)
+        if (delayLevel[keyLabel] >= delay)
         {
             return;
         }
-        delayLevel[directionLabel] = Mathf.Min(1f, delayLevel[directionLabel] + Time.deltaTime * chargeSpeed);
-        print(directionLabel + delayLevel[directionLabel]);
+        delayLevel[keyLabel] = Mathf.Min(1f, delayLevel[keyLabel] + Time.deltaTime * chargeSpeed);
+        print(controllerKey.keyCode.ToString() + delayLevel[keyLabel]);
     }
 }
 
-[CustomEditor(typeof(PlayerMovement))]
-public class PlayerMovementEditor : Editor
+public enum KeyLabel { up, left, down, right, sprint };
+
+[System.Serializable]
+public class ControllerKey
 {
-    override public void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        var myScript = target as PlayerMovement;
-        if (myScript.enableDelay)
-        {
-            myScript.delayUp = GUILayout.Toggle(myScript.delayUp, "Delay Up");
-            myScript.delayLeft = GUILayout.Toggle(myScript.delayLeft, "Delay Left");
-            myScript.delayDown = GUILayout.Toggle(myScript.delayDown, "Delay Down");
-            myScript.delayRight = GUILayout.Toggle(myScript.delayRight, "Delay Right");
-        }
-    }
+    public KeyLabel keyLabel;
+    public KeyCode keyCode;
+    public bool delayEnabled;
+
+    [HideInInspector]
+    public Vector3 direction;
 }
