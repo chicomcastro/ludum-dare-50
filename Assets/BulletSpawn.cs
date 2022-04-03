03dcc98dfd54f68ccd212fe98ec7b2c032f7c47f
Francisco Castro <francisco.castro@sooper.com.br>
2022-04-03 20:56:40 -0300

    Bullet controller

---
+++ Assets/BulletSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    public Transform spawnSpot;
+
+    public float initialDelay = 1f;
+    public float shootPeriod = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnBullet", initialDelay, shootPeriod);
+    }
+
+    private void SpawnBullet()
+    {
+        GameObject gamo = Instantiate(bulletPrefab, spawnSpot.position, spawnSpot.rotation, transform);
+        Destroy(gamo, 30f);
+    }
+}
---
+++ Assets/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 5f;
+    public float destroyDelay = 0.1f;
+
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = speed * transform.forward;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+}
