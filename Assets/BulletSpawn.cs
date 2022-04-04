7799d4082b73cb820eb549eb32b97c3de5ea1e51
Francisco Castro <francisco.castro@sooper.com.br>
2022-04-03 23:32:24 -0300

    Include condition to stop shooting

--- Assets/BulletSpawn.cs
+++ Assets/BulletSpawn.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class BulletSpawn : MonoBehaviour
 {
     public GameObject bulletPrefab;
-
     public Transform spawnSpot;
 
     public float initialDelay = 1f;
     public float shootPeriod = 0.5f;
 
-    // Start is called before the first frame update
+    private FollowBehaviour followBehaviour;
+
     void Start()
     {
+        followBehaviour = GetComponent<FollowBehaviour>();
         InvokeRepeating("SpawnBullet", initialDelay, shootPeriod);
     }
 
     private void SpawnBullet()
+    {
+        if (!followBehaviour.isAway())
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
     {
         GameObject gamo = Instantiate(bulletPrefab, spawnSpot.position, spawnSpot.rotation);
         Destroy(gamo, 30f);
