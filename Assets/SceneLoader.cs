fb84f55e95e6dbb3b2f950e8e8660aa343d7e24e
Francisco Castro <francisco.castro@sooper.com.br>
2022-04-04 19:28:46 -0300

    Implement scene management

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadCurrentScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
