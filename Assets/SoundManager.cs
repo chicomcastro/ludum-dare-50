bea1443c2a3c5395e7550bc5587c6d28ca073c06
Francisco Castro <francisco.castro@sooper.com.br>
2022-04-04 21:44:28 -0300

    Add music!

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
